16467bf538ed224be5700da909a3e990e292469a
ahmadnasir123 <nasirahmad240502@gmail.com>
2023-09-27 21:39:22 +0700

    init commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/css/main.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -32,7 +34,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxt/postcss8',
     '@nuxtjs/dotenv',
   ],
 
@@ -50,5 +52,12 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+},
 }
---
+++ pages/companies.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="py-[200px] flex flex-col items-center justify-center px-4"> 
+        <div class="text-[32px] font-semibold text-dark mb-4"> Select Companies</div>
+        <form class="w-full card">
+            <div class="form-group">
+                <label for="" class="text-grey">Companies</label>
+                <select name="" id="" class="appearance-none input-field form-icon-chevron_down">
+                    <option value="" selected>Company Name</option>
+                </select>
+            </div>
+            <a href="/companies/1" class="w-full btn btn-primary mt-[14px]">Continue</a>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {}
+</script>
